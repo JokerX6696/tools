03016cdfa0e7d13eba202dc9c634e16cf920f508
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2023-05-18 15:36:27 +0800

    conda 配置文件

---
+++ conda_config.sh
@@ -0,0 +1,14 @@
+channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/pro
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - bioconda
+  - conda-forge
+  - defaults
+show_channel_urls: true
+envs_dirs:
+  - /data/USER/zhengfuxing/conda
+pkgs_dirs:
+  - /data/USER/zhengfuxing/conda
