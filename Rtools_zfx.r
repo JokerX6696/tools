40e44e937de6aa40ac36b458b9924d0c766454f6
JokerX6696 <313445042@qq.com>
2023-12-15 18:11:19 +0800

    write.table 输出表格解决错位问题

---
+++ Rtools_zfx.r
@@ -0,0 +1,11 @@
+# 使用 R 语言过程中 可以提升效率的部分小工具
+
+
+# 用于输出数据write.table时， 首行缺失或错位的问题 返回矩阵后需要手动给第一列行名赋值
+normalize_mtx <- function(Mtx){
+  df <- Mtx
+  rnm <- rownames(df)
+  new_df <- data.frame(name=rnm,df)
+  names(new_df)[1] = ""
+  return(new_df)
+}
