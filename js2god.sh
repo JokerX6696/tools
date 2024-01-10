b756d3c8a6ab057482aeeeeba43114619c180ffb
JokerX6696 <313445042@qq.com>
2024-01-10 15:37:58 +0800

    修正技术支持给的注释不规范的问题

---
+++ js2god.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+jsfile=$1
+houzhui=$(echo $jsfile|awk -F "." '{print $NF}')
+if [[ $houzhui == 'csv' ]];then
+	fengefu=','
+elif [[ $houzhui == 'tsv' ]];then
+	fengefu='\t'
+fi
+
+paste -d "${fengefu}" \
+<(cut -f1 -d "${fengefu}" ${jsfile}) \
+<(cut -f2  ${jsfile} -d "${fengefu}"| \
+sed 's/\s\s*/_/g;s/&/_/g;s/+/_/g;s/-/_/g'|sed 's/__*/_/g') > new_celltype_Standard.${houzhui}
+
