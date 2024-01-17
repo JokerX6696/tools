49978e2ef608813f9431bdbf64f8abdb22e2cd7f
JokerX6696 <313445042@qq.com>
2024-01-17 17:18:08 +0800

    添加斜线

--- js2god.sh
+++ js2god.sh
@@ -3,13 +3,13 @@ set -e
 jsfile=$1
 houzhui=$(echo $jsfile|awk -F "." '{print $NF}')
 if [[ $houzhui == 'csv' ]];then
-	fengefu=','
+        fengefu=','
 elif [[ $houzhui == 'tsv' ]];then
-	fengefu='\t'
+        fengefu='\t'
 fi
 
 paste -d "${fengefu}" \
 <(cut -f1 -d "${fengefu}" ${jsfile}) \
 <(cut -f2  ${jsfile} -d "${fengefu}"| \
-sed 's/^\s\s*//g;s/\s\s*$//g;s/\s\s*/_/g;s/&/_/g;s/+/_/g;s/-/_/g'|sed 's/__*/_/g') > new_celltype_Standard.${houzhui}
+sed 's|/|_|g;s/^\s\s*//g;s/\s\s*$//g;s/\s\s*/_/g;s/&/_/g;s/+/_/g;s/-/_/g'|sed 's/__*/_/g') > new_celltype_Standard.${houzhui}
 
