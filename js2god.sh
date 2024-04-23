ece23fc86100707385f6eb72279ca0fd9c0681b1
JokerX6696 <313445042@qq.com>
2024-04-23 16:17:13 +0800

    半角转全角

--- js2god.sh
+++ js2god.sh
@@ -13,3 +13,30 @@ paste -d "${fengefu}" \
 <(cut -f2  ${jsfile} -d "${fengefu}"| \
 sed 's|/|_|g;s/^\s\s*//g;s/\s\s*$//g;s/\s\s*/_/g;s/&/_/g;s/+/_/g;s/-/_/g'|sed 's/__*/_/g') > new_celltype_Standard.${houzhui}
 
+# def strQ2B(ustring):
+#     """全角转半角"""
+#     rstring = ""
+#     for uchar in ustring:
+#         inside_code = ord(uchar)
+#         print(inside_code)
+#         if inside_code == 12288:  # 全角空格直接转换
+#             inside_code = 32
+#         elif 65281 <= inside_code <= 65374:  # 全角字符（除空格）根据关系转化
+#             inside_code -= 65248
+ 
+#         rstring += unichr(inside_code)
+#     return rstring
+ 
+ 
+# def strB2Q(ustring):
+#     """半角转全角"""
+#     rstring = ""
+#     for uchar in ustring:
+#         inside_code = ord(uchar)
+#         if inside_code == 32:  # 半角空格直接转化
+#             inside_code = 12288
+#         elif 32 <= inside_code <= 126:  # 半角字符（除空格）根据关系转化
+#             inside_code += 65248
+ 
+#         rstring += unichr(inside_code)
+#     return rstring
